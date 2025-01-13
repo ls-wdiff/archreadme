9be350e1e6d95fd5674383fab646c3b17d501183
ls-wdiff <nanotech64@gmail.com>
2025-01-13 01:47:32 -0800

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 install_paru() {
     if ! command -v paru &> /dev/null; then
         echo "Installing paru..."
@@ -8,6 +6,7 @@ install_paru() {
         cd paru-bin || exit
         makepkg -si --noconfirm
         cd .. && rm -rf paru-bin
+        mkdir -p "$HOME/.local/bin"
         export PATH="$PATH:$HOME/.local/bin"
     else
         echo "paru is already installed."
@@ -15,7 +14,7 @@ install_paru() {
 }
 install_kde() {
     echo "Installing KDE Plasma, Drivers, Utilities, Wine and wine related packages..."
-    sudo pacman -S --noconfirm plasma-desktop dolphin konsole systemsettings plasma-pa plasma-nm kscreen kde-gtk-config breeze-gtk powerdevil sddm-kcm kwalletmanager kio-admin bluedevil ark mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader steam lutris wine-staging winetricks gamemode lib32-gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader mangohud lib32-mangohud goverlay gamescope bluez bluez-utils lib32-libpulse xwaylandvideobridge
+sudo pacman -S --noconfirm plasma-desktop dolphin konsole systemsettings plasma-pa plasma-nm kscreen kde-gtk-config breeze-gtk powerdevil sddm-kcm kwalletmanager kio-admin bluedevil ark mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader steam lutris wine-staging winetricks gamemode lib32-gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader mangohud lib32-mangohud goverlay gamescope bluez bluez-utils lib32-libpulse xwaylandvideobridge
 
 sudo systemctl enable NetworkManager 
 sudo systemctl enable bluetooth.service
@@ -27,5 +26,10 @@ install_kernel() {
       MAKEFLAGS="-j$(nproc)" paru -S --noconfirm linux-lqx linux-lqx-headers
       sudo grub-mkconfig -o /boot/grub/grub.cfg
       echo "Reboot"
-      break
+      sudo reboot
 }
+
+# Execute functions
+install_paru
+install_kde
+install_kernel
